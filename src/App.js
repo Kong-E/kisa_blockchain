bd5d3e47811de6037d4a22507c9e66c62fcead8c
Kong-E <kng001016@dgu.ac.kr>
2023-08-30 14:03:22 +0900

    chore: printObject 함수 추가

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ function App() {
   const [address, setAddress] = useState(null);
   const [balance, setBalance] = useState(null);
   const [block, setBlock] = useState(null);
+  const [transaction, setTransaction] = useState(null);
 
   const loadTransaction = async () => {
     const transaction = await web3.eth.getTransaction(
       "0xbc6fa9fec2e9892b639561662e62fcc4aecedacb1b7c867b2108d3116550de8c"
     ); // Transaction Hash를 넣어야 함
     console.log("transaction :", transaction);
+    setTransaction(transaction);
   };
 
   const loadBlock = async () => {
@@ -33,6 +35,12 @@ function App() {
     const balance = await web3.eth.getBalance(account.address);
     const ether = web3.utils.fromWei(balance, "ether");
     setBalance(ether);
+  };
+
+  const printObject = (data) => {
+    return JSON.stringify(data, (key, value) => {
+      return typeof value === "bigint" ? value.toString() : value;
+    });
   };
 
   useEffect(() => {
@@ -58,7 +66,8 @@ function App() {
     <>
       <div className="App">내 지갑주소는 {address}</div>
       <div className="App">보유 코인 : {balance}</div>
-      {/* <div className="App">최근 블록 : {block}</div> */}
+      {/* <div className="App">최근 블록 : {printObject(block)}</div>
+      <div className="App">트랜잭션 : {printObject(transaction)}</div> */}
     </>
   );
 }
