7d34b739043f532bf14a1285a38750467ae11866
Kong-E <kng001016@dgu.ac.kr>
2023-08-30 13:43:22 +0900

    feat: block과 transaction 불러오기

--- src/App.js
+++ src/App.js
@@ -7,11 +7,28 @@ function App() {
   const [web3, setWeb3] = useState(null);
   const [address, setAddress] = useState(null);
   const [balance, setBalance] = useState(null);
+  const [block, setBlock] = useState(null);
+
+  const loadTransaction = async () => {
+    const transaction = await web3.eth.getTransaction(
+      "0xbc6fa9fec2e9892b639561662e62fcc4aecedacb1b7c867b2108d3116550de8c"
+    ); // Transaction Hash를 넣어야 함
+    console.log("transaction :", transaction);
+  };
+
+  const loadBlock = async () => {
+    const number = await web3.eth.getBlockNumber();
+    const block = await web3.eth.getBlock();
+    console.log("number :", number);
+    console.log("block :", block);
+    setBlock(block);
+  };
 
   const loadBalance = async () => {
     const account = await web3.eth.accounts.privateKeyToAccount(
       process.env.REACT_APP_PRIVATE_KEY
     );
+    console.log("account : ", account);
     setAddress(account.address);
     const balance = await web3.eth.getBalance(account.address);
     const ether = web3.utils.fromWei(balance, "ether");
@@ -22,20 +39,26 @@ function App() {
     if (!process.env.REACT_APP_INFURA_ENDPOINT) {
       throw new Error("REACT_APP_INFURA_ENDPOINT not found");
     }
-    const web3Instance = new Web3(
-      new Web3.providers.HttpProvider(process.env.REACT_APP_INFURA_ENDPOINT)
+    const provider = new Web3.providers.HttpProvider(
+      process.env.REACT_APP_INFURA_ENDPOINT
     );
+    const web3Instance = new Web3(provider);
     setWeb3(web3Instance);
   }, []);
 
   useEffect(() => {
-    if (web3) loadBalance();
+    if (web3) {
+      loadBalance();
+      loadBlock();
+      loadTransaction();
+    }
   }, [web3]);
 
   return (
     <>
       <div className="App">내 지갑주소는 {address}</div>
       <div className="App">보유 코인 : {balance}</div>
+      {/* <div className="App">최근 블록 : {block}</div> */}
     </>
   );
 }
