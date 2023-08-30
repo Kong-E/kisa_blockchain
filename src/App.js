8d72059d0416c8129ebaaa003eebaeea2204220c
Kong-E <kng001016@dgu.ac.kr>
2023-08-30 12:00:30 +0900

    feat : infura 엔드포인트 연결 및 account 가져오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Web3 from "web3";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [web3, setWeb3] = useState(null);
+  const [address, setAddress] = useState(null);
+
+  const loadBalance = async () => {
+    const account = await web3.eth.accounts.privateKeyToAccount(
+      process.env.REACT_APP_PRIVATE_KEY
+    );
+    setAddress(account.address);
+  };
+
+  useEffect(() => {
+    if (!process.env.REACT_APP_INFURA_ENDPOINT) {
+      throw new Error("REACT_APP_INFURA_ENDPOINT not found");
+    }
+    const web3Instance = new Web3(
+      new Web3.providers.HttpProvider(process.env.REACT_APP_INFURA_ENDPOINT)
+    );
+    setWeb3(web3Instance);
+  }, []);
+
+  useEffect(() => {
+    if (web3) loadBalance();
+  }, [web3]);
+
+  return <div className="App">내 지갑주소는 {address}</div>;
 }
 
 export default App;
